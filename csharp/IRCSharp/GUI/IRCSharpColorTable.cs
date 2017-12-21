38d7a716ff7c52d8c25c3ec91f03250e25ad23dc
Rob N <robnw@null.net>
2017-12-20 16:11:05 -0800

    add ircsharp sln and add basic gui to project (no functionality, we're getting there)

---
+++ csharp/IRCSharp/GUI/IRCSharpColorTable.cs
@@ -0,0 +1,77 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace IRCSharp
+{
+    class IRCSharpColorTable : ProfessionalColorTable
+    {
+        public IRCSharpColorTable()
+        {
+            base.UseSystemColors = false;
+        }
+
+        #region Toolstrip Colors
+        public override Color ToolStripBorder
+        {
+            get { return Color.Black; }
+        }
+        public override Color GripDark
+        {
+            get { return Color.Black; }
+        }
+        public override Color GripLight
+        {
+            get { return Color.Black; }
+        }
+        public override Color ToolStripGradientBegin
+        {
+            get { return Color.Black; }
+        }
+        public override Color ToolStripGradientMiddle
+        {
+            get { return Color.Black; }
+        }
+        public override Color ToolStripGradientEnd
+        {
+            get { return Color.Black; }
+        }
+        public override Color ToolStripContentPanelGradientEnd
+        {
+            get { return Color.Black; }
+        }
+        public override Color ToolStripContentPanelGradientBegin
+        {
+            get { return Color.Black; }
+        }
+        #endregion
+
+        #region Dropdown Colors
+        //These change the colors of the borders and the overall background color of the dropdown menu.
+        public override Color MenuBorder
+        {
+            get { return Color.DarkGreen; }
+        }
+        public override Color MenuItemBorder
+        {
+            get { return Color.DarkGreen; }
+        }
+        public override Color ToolStripDropDownBackground
+        {
+            get { return Color.Black; }
+        }
+        //There is a small box to the right of the selections called an image margin. It has its own colors below.
+        public override Color ImageMarginGradientBegin
+        {
+            get { return Color.Black; }
+        }
+        public override Color ImageMarginGradientMiddle
+        {
+            get { return Color.Black; }
+        }
+        public override Color ImageMarginGradientEnd
+        {
+            get { return Color.Black; }
+        }
+        #endregion
+    }
+}
